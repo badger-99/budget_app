360ba344305bc68ce35e8f3af6d8801bcb2376bd
Alfred M <alfred.mkg@proton.me>
2023-11-06 22:20:42 +0300

    Fix invalid column key

--- db/migrate/20231106171214_create_categories.rb
+++ db/migrate/20231106171214_create_categories.rb
@@ -1,11 +1,13 @@
 class CreateCategories < ActiveRecord::Migration[7.1]
   def change
     create_table :categories do |t|
-      t.string :name, unique: true
+      t.string :name
       t.string :icon
       t.references :user, null: false, foreign_key: true, index: true
 
       t.timestamps
     end
+
+    add_index :categories, :name, unique: true
   end
 end
