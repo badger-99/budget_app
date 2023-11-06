61f4a3e6568a226537503f85fbb776650cbf5eb3
Alfred M <alfred.mkg@proton.me>
2023-11-06 22:01:09 +0300

    Add validation to models

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,16 @@
 class Payment < ApplicationRecord
   belongs_to :user
+  has_many :category_payments
+
+  validates :name, presence: true
+  validates :amount, presence: true
+  validate :at_least_one_category
+
+  private
+
+  def at_least_one_category
+    return unless category_payments.empty?
+
+    errors.add(:base, 'A payment must have at least one category')
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  has_many :categories
+  has_many :payments, foreign_key: 'author_id'
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
 end
