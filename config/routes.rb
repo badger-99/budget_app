c1b420c49703bfab323ed4071f55665573c8a883
Alfred M <alfred.mkg@proton.me>
2023-11-07 13:07:52 +0300

    Soft-remove unused 'user' resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
+  # resources :users
   resources :categories
   resources :payments
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
