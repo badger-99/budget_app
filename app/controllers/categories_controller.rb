1ef3c9b12b52642266bee82665c87323aaaca6a9
Alfred M <alfred.mkg@proton.me>
2023-11-08 18:42:03 +0300

    Clean-up controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -18,51 +18,8 @@ class CategoriesController < ApplicationController
     end
   end
 
-  # before_action :set_category, only: %i[show edit update destroy]
-
-  # GET /categories or /categories.json
-
-  # GET /categories/1 or /categories/1.json
-  # def show; end
-
-  # GET /categories/new
-
-  # GET /categories/1/edit
-  # def edit; end
-
-  # POST /categories or /categories.json
-
-  # PATCH/PUT /categories/1 or /categories/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @category.update(category_params)
-  #       format.html { redirect_to category_url(@category), notice: 'Category was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: @category }
-  #     else
-  #       format.html { render :edit, status: :unprocessable_entity }
-  #       format.json { render json: @category.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # DELETE /categories/1 or /categories/1.json
-  # def destroy
-  #   @category.destroy!
-
-  #   respond_to do |format|
-  #     format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
-  #     format.json { head :no_content }
-  #   end
-  # end
-
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  # def set_category
-  #   @category = Category.find(params[:id])
-  # end
-
-  # Only allow a list of trusted parameters through.
   def category_params
     params.require(:category).permit(:name, :icon, :user_id)
   end
--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,67 +1,29 @@
 class PaymentsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_payment, only: %i[show edit update destroy]
 
-  # GET /payments or /payments.json
   def index
     @category = Category.find(params[:category_id])
     @payments = Payment.all
   end
 
-  # GET /payments/1 or /payments/1.json
-  def show; end
-
-  # GET /payments/new
   def new
     @payment = Payment.new
   end
 
-  # GET /payments/1/edit
-  def edit; end
-
-  # POST /payments or /payments.json
   def create
     @payment = Payment.new(payment_params)
     category = Category.find(params[:category_id])
 
     if @payment.save
-      redirect_to "/categories/#{category.id}/category_payment/#{@payment.id}/new"
+      redirect_to "/categories/#{category.id}/category_payment/#{@payment}/new"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /payments/1 or /payments/1.json
-  def update
-    respond_to do |format|
-      if @payment.update(payment_params)
-        format.html { redirect_to payment_url(@payment), notice: 'Payment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @payment }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @payment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /payments/1 or /payments/1.json
-  def destroy
-    @payment.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to payments_url, notice: 'Payment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_payment
-    @payment = Payment.find(params[:id])
-  end
 
-  # Only allow a list of trusted parameters through.
   def payment_params
     params.require(:payment).permit(:name, :amount, :author_id)
   end
