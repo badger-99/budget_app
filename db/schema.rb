4c7ec846f6da1edf12aca4c8233185a20163cbce
Alfred M <alfred.mkg@proton.me>
2023-11-08 20:57:40 +0300

    Change amount column from decimal to float

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_06_175431) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_08_175008) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,7 +35,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_06_175431) do
 
   create_table "payments", force: :cascade do |t|
     t.string "name"
-    t.decimal "amount", precision: 9, scale: 2
+    t.float "amount"
     t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
