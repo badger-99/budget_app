8fb1c283094acb96f86ede5b4faf58ac1da3ffd4
Alfred M <alfred.mkg@proton.me>
2023-11-08 18:28:52 +0300

    Remove unused validation in payment model

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -4,13 +4,4 @@ class Payment < ApplicationRecord
 
   validates :name, presence: true
   validates :amount, presence: true
-  validate :at_least_one_category
-
-  private
-
-  def at_least_one_category
-    return unless category_payments.empty?
-
-    errors.add(:base, 'A payment must have at least one category')
-  end
 end
