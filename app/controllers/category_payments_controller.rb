e98a8413272e18ebb16db77bab38a42ec7cfa02d
Alfred M <alfred.mkg@proton.me>
2023-11-08 18:36:39 +0300

    Update create action in category_payment control

--- app/controllers/category_payments_controller.rb
+++ app/controllers/category_payments_controller.rb
@@ -4,21 +4,32 @@ class CategoryPaymentsController < ApplicationController
   def new
     @category_payment = CategoryPayment.new
     @categories = Category.where(user_id: current_user.id)
+    @payment = Payment.find(params[:payment_id])
+    @category = Category.find(params[:category_id])
   end
 
   def create
-    @category_payment = CategoryPayment.new(payment_params)
+    category = Category.find(params[:category_id])
+    category_ids = params[:category_ids]
+    success = true
 
-    if @category_payment.save
-      redirect_to payments_path, notice: 'Payment was successfully created.'
+    category_ids.each do |category_id|
+      @category_payment = CategoryPayment.new(category_id:, payment_id: params[:payment_id])
+      success = false unless @category_payment.save
+    end
+
+    if success
+      flash[:notice] = 'Category payment was successfully created.'
+      redirect_to "/categories/#{category.id}/payment"
     else
-      render :new, status: :unprocessable_entity
+      flash[:error] = 'Failed to create the category payment.'
+      render :new
     end
   end
 
   private
 
-  def payment_params
-    params.require(:payment).permit(:category_id, :payment_id)
+  def category_payment_params
+    params.require(:category_payment).permit(:payment_id, category_ids: [])
   end
 end
