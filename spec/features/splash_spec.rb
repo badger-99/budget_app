66ebd0f226700be5866d82445804e32d21d655d2
Alfred M <alfred.mkg@proton.me>
2023-11-09 20:42:22 +0300

    Add home-page spec

--- spec/features/splash_spec.rb
+++ spec/features/splash_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.feature 'Splash Page', type: :feature do
-  it 'Should render the unauthorized root (Splash) page' do
+RSpec.feature 'Splash Page (unauthenticated root)', type: :feature do
+  it 'Should render the unauthenticated root (Splash) page' do
     visit '/'
     expect(page).to have_content('MoneyMate')
     expect(page).to have_button('Log In')
