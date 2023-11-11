5fb088c1d620b1607a6bf0fcf13e5047f9e04a89
Alfred M <alfred.mkg@proton.me>
2023-11-12 00:16:48 +0300

    Update spec files

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -11,7 +11,7 @@ RSpec.feature 'Sign In page', type: :feature do
     it 'should log Me in' do
       fill_in 'Email', with: 'me@code.io'
       fill_in 'Password', with: 'f4k3p455w0rd'
-      click_button 'Log in'
+      click_button 'Log In'
       expect(page).to have_content 'Home'
     end
   end
